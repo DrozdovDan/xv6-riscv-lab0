7391517411d12893fe8ef3961e7fe007dae98e80
DanielDrozdov <daniil.drozdovjr@gmail.com>
2024-05-14 23:35:56 +0300

    commited suicide

--- user/test.c
+++ user/test.c
@@ -8,8 +8,6 @@
 #define N_CASES 10
 #define BUFSIZE 512
 
-char buf[BUFSIZE];
-
 char* cases[N_CASES] = {
     "000\n",
     "111\n",
@@ -23,11 +21,120 @@ char* cases[N_CASES] = {
     "999\n"
 };
 
+char*
+fmtname(char *path)
+{
+  static char buf[DIRSIZ+1];
+  char *p;
+
+  // Find first character after last slash.
+  for(p=path+strlen(path); p >= path && *p != '/'; p--)
+    ;
+  p++;
+
+  // Return blank-padded name.
+  if(strlen(p) >= DIRSIZ)
+    return p;
+  memmove(buf, p, strlen(p));
+  memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
+  return buf;
+}
+
+void
+ls(char *path)
+{
+  char buf[512], *p;
+  int fd;
+  struct dirent de;
+  struct stat st;
+
+  if((fd = open(path, O_NOFOLLOW)) < 0){
+    fprintf(2, "ls: cannot open %s\n", path);
+    return;
+  }
+
+  if(fstat(fd, &st) < 0){
+    fprintf(2, "ls: cannot stat %s\n", path);
+    close(fd);
+    return;
+  }
+
+  switch(st.type){
+  case T_DEVICE:
+  case T_FILE:
+    printf("%s %d %d %l\n", fmtname(path), st.type, st.ino, st.size);
+    break;
+
+  case T_DIR:
+    if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+      printf("ls: path too long\n");
+      break;
+    }
+    strcpy(buf, path);
+    p = buf+strlen(buf);
+    *p++ = '/';
+    while(read(fd, &de, sizeof(de)) == sizeof(de)){
+      if(de.inum == 0)
+        continue;
+      memmove(p, de.name, DIRSIZ);
+      p[DIRSIZ] = 0;
+      if(lstat(buf, &st) < 0){
+        printf("ls: cannot stat %s\n", buf);
+        continue;
+      }
+      if (st.type == T_SYMLINK) {
+        ls(buf);
+      } else {
+        printf("%s %d %d %d\n", fmtname(buf), st.type, st.ino, st.size);
+      }
+    }
+    break;
+
+  case T_SYMLINK:
+    char symlink_buf[MAXPATH + 1];
+    int n = readlink(path, symlink_buf);
+    if (n < 0) {
+      printf("ls: cannot read link %s\n", fmtname(path));
+    }
+    else {
+      symlink_buf[n] = 0;
+      printf("%s %d %d %l -> %s\n", fmtname(path), st.type, st.ino, st.size, symlink_buf);
+    }
+    break;
+  }
+  close(fd);
+}
+
 int generate_file(char* path, char* content) {
     int file = open(path, O_CREATE | O_RDWR);
     if (file < 0) return -1;
     write(file, content, sizeof(content));
     close(file);
+    return 0;
+}
+
+int check_link(char* link, char* content) {
+    char path[BUFSIZE];
+    char* defpath = "/d1/d2/d3/";
+    char buf[BUFSIZE];
+    int n = strlen(defpath);
+    for (int i = 0; i < n; i++)
+        path[i] = defpath[i];
+    for (int i = 0; i < strlen(link); i++) 
+        path[i + n] = link[i];
+    int file = open(path, O_RDWR);
+    if (file < 0) {
+        if (!content)
+            return 0;
+        return 1;
+    }
+    if (!content)
+        return 1;
+    n = read(file, buf, BUFSIZE);
+    if (n < 0)
+        return 1;
+    if (strcmp(buf, content) != 0) 
+        return 1;
     return 0;
 }
 
@@ -116,12 +223,12 @@ int main(int argc, char const *argv[])
         fprintf(2, "ERROR...\n");
         exit(1);
     }
-    n = symlink("/d1/d2/d3/l9", "/d1/d2/d3/l10");
+    n = symlink("/d1/d2/d3/l10", "/d1/d2/d3/l9");
     if (n < 0) {
         fprintf(2, "ERROR...\n");
         exit(1);
     }
-    n = symlink("/d1/d2/d3/l10", "/d1/d2/d3/l9");
+    n = symlink("/d1/d2/d3/l9", "/d1/d2/d3/l10");
     if (n < 0) {
         fprintf(2, "ERROR...\n");
         exit(1);
@@ -146,5 +253,90 @@ int main(int argc, char const *argv[])
         fprintf(2, "ERROR...\n");
         exit(1);
     }
+
+    printf("=/=\n");
+    ls("/");
+    printf("=/d1=\n");
+    ls("/d1");
+    printf("=/d1/d2=\n");
+    ls("/d1/d2");
+    printf("=/d1/d2/d3=\n");
+    ls("/d1/d2/d3");
+    printf("=/d1/d2/d3/d4=\n");
+    ls("/d1/d2/d3/d4");
+    printf("=/d1/d2/d3/d4/d5=\n");
+    ls("/d1/d2/d3/d4/d5");
+
+    n = check_link("l1", cases[0]);
+    if (n != 0) {
+        fprintf(2, "1 went wrong\n");
+        exit(1);
+    }
+    n = check_link("l2", cases[0]);
+    if (n != 0) {
+        fprintf(2, "2 went wrong\n");
+        exit(1);
+    }
+    n = check_link("l3", cases[2]);
+    if (n != 0) {
+        fprintf(2, "3 went wrong\n");
+        exit(1);
+    }
+    n = check_link("l4", cases[1]);
+    if (n != 0) {
+        fprintf(2, "4 went wrong\n");
+        exit(1);
+    }
+    n = check_link("l5", cases[0]);
+    if (n != 0) {
+        fprintf(2, "5 went wrong\n");
+        exit(1);
+    }
+    n = check_link("l6", cases[0]);
+    if (n != 0) {
+        fprintf(2, "6 went wrong\n");
+        exit(1);
+    }
+    n = check_link("l7", cases[0]);
+    if (n != 0) {
+        fprintf(2, "7 went wrong\n");
+        exit(1);
+    }
+    /*n = check_link("l8", 0);
+    if (n != 0) {
+        fprintf(2, "8 went wrong\n");
+        exit(1);
+    }
+    n = check_link("l9", 0);
+    if (n != 0) {
+        fprintf(2, "9 went wrong\n");
+        exit(1);
+    }
+    n = check_link("l10", 0);
+    if (n != 0) {
+        fprintf(2, "10 went wrong\n");
+        exit(1);
+    }*/
+    n = check_link("l11", 0);
+    if (n != 0) {
+        fprintf(2, "11 went wrong\n");
+        exit(1);
+    }
+    n = check_link("l12", 0);
+    if (n != 0) {
+        fprintf(2, "12 went wrong\n");
+        exit(1);
+    }
+    n = check_link("l13", 0);
+    if (n != 0) {
+        fprintf(2, "13 went wrong\n");
+        exit(1);
+    }
+    n = check_link("l14", 0);
+    if (n != 0) {
+        fprintf(2, "14 went wrong\n");
+        exit(1);
+    }
+    printf("It is done!\n");
     exit(0);
 }
